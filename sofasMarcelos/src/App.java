b30301935c99bc5fc58df3021f172f62e6512507
Pedro-Aron <pedroaronfig@gmail.com>
2023-07-01 20:47:47 -0300

    javafx, scenebuilder e launch.json

--- sofasMarcelos/src/App.java
+++ sofasMarcelos/src/App.java
@@ -1,6 +1,20 @@
-public class App  {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello!!");
-        System.out.println("Teste commit");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final FXMLLoader telaLoginXml = new FXMLLoader(getClass().getResource("telaLogin.fxml"));
+        Scene telaLoginTela = new Scene(telaLoginXml.load());
+
+        primaryStage.setTitle("Log-in e cadastro");
+        primaryStage.setScene(telaLoginTela);
+        primaryStage.show();
     }
 }
