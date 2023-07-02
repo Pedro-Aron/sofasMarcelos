ebc3c156c9cf03bee969cd5ab7e6f70175d73056
Pedro-Aron <pedroaronfig@gmail.com>
2023-07-01 21:36:30 -0300

    mais testes com javafx

--- sofasMarcelos/src/App.java
+++ sofasMarcelos/src/App.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -11,10 +12,19 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         final FXMLLoader telaLoginXml = new FXMLLoader(getClass().getResource("telaLogin.fxml"));
-        Scene telaLoginTela = new Scene(telaLoginXml.load());
+        Scene telaLoginCena = new Scene(telaLoginXml.load());
 
         primaryStage.setTitle("Log-in e cadastro");
-        primaryStage.setScene(telaLoginTela);
+        primaryStage.setScene(telaLoginCena);
         primaryStage.show();
+
+        Button botaoLogin = (Button) telaLoginCena.lookup("#botaoCadastroTela1");
+        TextField campoEmail = (TextField) telaLoginCena.lookup("#campoEmailTela1");
+        PasswordField campoSenha = (PasswordField) telaLoginCena.lookup("#campoSenhaTela1");
+
+        botaoLogin.setOnAction((e) -> {
+            System.out.println(campoEmail.getText());
+            System.out.println(campoSenha.getText());
+        });
     }
 }
