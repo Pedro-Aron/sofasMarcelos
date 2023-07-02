48c674d92be96a4f7c047d9dc3a0f7adf08ced78
Felipe-de-Carvalho-Andrade <felipea07112006@gmail.com>
2023-07-01 21:53:13 -0300

    Beto_Teste

--- sofasMarcelos/src/App.java
+++ sofasMarcelos/src/App.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
