d7a4519a9a7d43298b2a90c238120066af1e0755
opedromeireles <pedrinhobelezaa@gmail.com>
2023-07-29 14:08:29 -0300

    link esqueceu senha sem funcionalidade tudo 2 de nike, comprei a vista

--- sofasMarcelos/src/UserLogin/loginController.java
+++ sofasMarcelos/src/UserLogin/loginController.java
@@ -1,6 +1,7 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -37,5 +38,12 @@ public class loginController {
             System.out.println(resultadoLogin);
 
     }
+    
+    @FXML
+    private Hyperlink link_esqueceu_senha;
 
+    @FXML
+    void esqueceu_senha(ActionEvent event) {
+
+    }
 }
