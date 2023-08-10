8b121912fc6cbed5d11c6e181cb9285bca7f8df6
Pedro-Aron <pedroaronfig@gmail.com>
2023-08-09 23:05:15 -0300

    import inutil removido

--- sofasMarcelos/src/TelaProdutosController.java
+++ sofasMarcelos/src/TelaProdutosController.java
@@ -1,7 +1,6 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
 
